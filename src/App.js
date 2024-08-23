8070ea554f849d2a8c0b158b1f198f1f49025ae7
Sean-K-ORourke-portfolio <orourke.sean.k@gmail.com>
2024-08-23 17:20:53 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import logo from './logo.svg';
+import profileImage from './portfolioImage.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={profileImage} className="App-logo" alt="myProfileImage" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my portfolio!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://whatever-i-want-channel.itch.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          my itch.io
+        </a>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/seán-o-rourke-521290246/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          my linkedin
+        </a>
+        <a
+          className="App-link"
+          href="https://github.com/Sean-K-ORourke-portfolio"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          my github
         </a>
       </header>
     </div>
